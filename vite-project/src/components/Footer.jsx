b7ecda9a900a23e342824ebb11add55f9a64c36c
alanaainez <alainez649@gmail.com>
2025-02-17 14:43:26 -0500

    git commit -m "Developed Footer and imported to App"

---
+++ vite-project/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 My Website. All rights reserved.</p>
+      <div>
+        <a href="https://github.com/alanaainez" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://www.linkedin.com/in/alana-rodriguez-473abb35/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
