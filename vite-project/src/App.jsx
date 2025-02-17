c58b95e6097b726867b8e4e48118e38daa00eb46
alanaainez <alainez649@gmail.com>
2025-02-17 16:10:22 -0500

    git commit -m "Added file for projects"

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
             <ul>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About Me</a></li>
-                <li><a href="#projects">Projects</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
             </nav>
