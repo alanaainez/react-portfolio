157cf818de4aa827023c6b660d072ab1e0074d28
alanaainez <alainez649@gmail.com>
2025-02-20 17:28:17 -0500

    git commit -m "Link import"

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,13 +3,11 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import About from './components/About'
-import Portfolio from './components/Portfolio'
+import Portfolio from './pages/Portfolio'
 import Contact from './components/Contact'
-import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
---
+++ vite-project/src/pages/Portfolio.jsx
@@ -0,0 +1,12 @@
+import Projects from "../components/Project";
+
+function Portfolio() {
+    return (
+        <main>
+            <h1>My Portfolio</h1>
+            <Projects />
+        </main>
+    );
+}
+
+export default Portfolio;
