6fd08a4ff421760abc28f6a87d8c34d8f372b127
alanaainez <alainez649@gmail.com>
2025-02-19 22:21:37 -0500

    git commit -m "Added about and contact pages"

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -13,24 +13,14 @@ function App() {
     <>
     <Router>
       <Header />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
       <Footer />
       </Router>
     </>
-  )
+  );
 }
 
 export default App
