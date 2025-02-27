a931835279e525629a65fea76c954c94d4a17316
alanaainez <alainez649@gmail.com>
2025-02-27 02:05:32 -0500

    git commit -m "Added image links"

--- vite-project/src/data/projectsData.js
+++ vite-project/src/data/projectsData.js
@@ -3,41 +3,45 @@ const projects = [
         id: 1,
         title: "Employee Tracker",
         description: "A program utilizing Node.js, Inquirer, and MySQL to manage a company's employee database.",
-        image: "https://via.placeholder.com/300",  // Replace with actual image URL
-        deployedLink: "https://your-live-project-link.com",
-        githubLink: "https://github.com/your-username/project-one"
+        image: "",
+        githubLink: "https://github.com/alanaainez/sql-employee-tracker.git",
     },
     {
         id: 2,
-        title: "Self-Care Survey",
-        description: "Survey application using Bulma CSS framework.",
-        image: "https://via.placeholder.com/300",
-        deployedLink: "https://your-live-project-link.com",
-        githubLink: "https://github.com/your-username/project-two"
+        title: "README Generator",
+        description: "",
+        githubLink: "https://github.com/alanaainez/readme-generator.git",
     },
     {
         id: 3,
-        title: "Tribute Page",
-        description: "",
-        image: "https://via.placeholder.com/300",
-        deployedLink: "https://your-live-project-link.com",
-        githubLink: ""
+        title: "Self-Care Survey",
+        description: "Survey application using Bulma CSS framework.",
+        image: "./src/assets/self-care.jpeg",
+        deployedLink: "https://alanaainez.github.io/self-care-survey/",
+        githubLink: "https://github.com/alanaainez/self-care-survey.git",
     },
     {
         id: 4,
-        title: "Vehicle Builder",
-        description: "A web application that allows users to build and control their own vehicles.",
+        title: "Tribute Page",
+        description: "",
+        image: "./src/assets/sensei-funakoshi.jpeg",
+        deployedLink: "https://your-live-project-link.com",
+        githubLink: "",
     },
     {
         id: 5,
-        title: "Weather Dashboard",
-        description: "A weather dashboard application that provides weather information for various cities.",
+        title: "Vehicle Builder",
+        description: "A web application that allows users to build and control their own vehicles.",
+        githubLink: "https://alanaainez.github.io/vehicle-builder/",
     },
     {
         id: 6,
-        title: "",
-        description: "",
-    }
+        title: "Weather Dashboard",
+        description: "A weather dashboard application that provides weather information for various cities.",
+        image: "./src/assets/weather-example.jpg",
+        deployedLink: "https://weather-dashboard-fqw6.onrender.com/",
+        githubLink: "https://github.com/alanaainez/weather-dashboard.git",
+    },
 ];
 
 export default projects;
