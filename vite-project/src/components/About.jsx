8e86f6b5c65aa21f325921863f2367cc76f33724
alanaainez <alainez649@gmail.com>
2025-03-01 12:58:51 -0500

    git commit -m "Updated About Me"

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -6,15 +6,10 @@ function About() {
       <div className="about-container">
         <img src={myImage} alt="My profile" className="about-image" />
         <p className="about-text">
-            Born and raised in Westchester County in NY, I'm an alumna of Wesleyan University 
-            and have a background in childcare and education. I first dipped my feet into the 
-            coding waters while I was stuck at home like many other people during the lockdown 
-            of 2020. During that time I gained knowledge and confidence learning how to build 
-            web pages, understanding JavaScript and making a temperature converter in Python 
-            with the help of online resources. It was fascinating to create something (I've 
-            always liked arts and crafts) and to gain inspiration to figure out kinks I came 
-            across in my code. After returning to the job field in office administration and 
-            doing some research, I've taken the plunge into computer programming.
+            Hello world! I'm a programer completing a Full-Stack bootcamp. I have a passion for creating tangible solutions to real-world problems.
+            I have a strong foundation in HTML, CSS, and JavaScript, and I'm always eager to learn new technologies and frameworks.
+            I enjoy finding various ways to solve problems and am always looking for new challenges.
+            In my free time, I enjoy video games, crochet, Gundam model building, and playing with my cat Luca.
             </p>
     </div>
   );
